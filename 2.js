b42cfe0288d3cd52b7b8d7b9b448414ea3120bd0
guilherme.dine <108938727+Guilhermedine@users.noreply.github.com>
2023-01-02 11:54:13 -0300

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,10 @@
+var salario = 1500
+var prestaçao = 150
+// const valor imultavel
+const maximo = salario * 0.3
+if (prestaçao > maximo) {
+    console.log("não conceder")
+} else {
+    console.log("conceder")
+}
+
