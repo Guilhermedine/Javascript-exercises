355e0352933934fdbb79024bf9a24c097696136f
guilherme.dine <108938727+Guilhermedine@users.noreply.github.com>
2023-01-09 14:18:02 -0300

    Add files via upload

---
+++ 16.js
@@ -0,0 +1,10 @@
+var n = 109
+if (n > 0 && n <= 30) {
+    console.log('esta entre 0 e 30')
+}
+else if (n > 40 && n <= 79){
+    console.log('esta entre 40 e 79')
+}
+else {
+    console.log('esta fora dos limites estabelecidos')
+}
