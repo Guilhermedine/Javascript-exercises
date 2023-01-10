aa1292b6270016a2a492b2f48ba68b69cf21cce1
guilherme.dine <108938727+Guilhermedine@users.noreply.github.com>
2023-01-10 01:34:51 -0300

    Add files via upload

---
+++ 17.js
@@ -0,0 +1,10 @@
+var valProduto = 5
+function valorVenda () {
+if (valProduto <= 20) {
+        return valProduto * 1.45;
+}
+else {
+        return valProduto * 1.30;
+}}
+console.log(valorVenda());
+    
