9bf1125393e5f060e142fe18af1b3ef3f4b21445
guilherme.dine <108938727+Guilhermedine@users.noreply.github.com>
2023-01-20 12:09:24 -0300

    Add files via upload

---
+++ 24.js
@@ -0,0 +1,12 @@
+var ida = [ 15 , 18 , 32 , 29 , 31 , 59 , 63 , 78 , 81 , 21]
+var n = 1
+var soma = 0
+while (n <= ida.length) {
+       soma += ida[n - 1]  
+       n += 1 
+}
+console.log(soma)
+console.log(soma / ida.length)
+
+
+
