c080094b6fafc6e48dc4b5546e0dbb4b40e49dd4
guilherme.dine <108938727+Guilhermedine@users.noreply.github.com>
2023-02-03 15:08:40 -0300

    Add files via upload

---
+++ 27.js
@@ -0,0 +1,33 @@
+let whole = 0
+while (whole <= 9) {
+    whole++
+    console.log(whole)
+}
+
+let odd = 1
+while (odd <= 17){
+    if (odd % 2 !== 0) 
+    console.log(odd)
+    odd = odd + 1
+}
+
+let pair = 1
+while (pair <= 10){
+    if (pair % 2 == 0) 
+    console.log(pair)
+    pair = pair + 1
+}
+
+let desc = 21
+while (desc >= 15) {
+    console.log(desc)
+    desc--
+}
+
+let j = 3
+let n = 0
+while (n <= 10) {
+    console.log(j + 'x' + n + '=' + (j*n))  
+    n++
+}
+
